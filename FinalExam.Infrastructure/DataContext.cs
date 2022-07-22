988c49f3c0604e6ddb77301e3ee7dc557097ee0a
Tornike Oniani <dragonforwar@gmail.com>
2022-07-22 22:08:24 +0400

    installed and configured EntityFramework

---
+++ FinalExam.Infrastructure/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using FinalExam.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalExam.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- FinalExam/Server/Program.cs
+++ FinalExam/Server/Program.cs
@@ -1,7 +1,14 @@
+using FinalExam.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
