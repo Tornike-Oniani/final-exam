1560a5c49006929f2beac9d344123ba153716dd5
Tornike Oniani <dragonforwar@gmail.com>
2022-07-22 22:41:15 +0400

    implemented fetching products from database

---
+++ FinalExam/Server/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalExam.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		private readonly DataContext _context;
+
+		public ProductController(DataContext context)
+		{
+			this._context = context;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Get()
+		{
+			var products = await _context.Products.ToListAsync();
+			return Ok(products);
+		}
+	}
+}
--- FinalExam/Server/Program.cs
+++ FinalExam/Server/Program.cs
@@ -1,11 +1,11 @@
+global using FinalExam.Domain.Models;
+global using FinalExam.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//builder.Services.AddDbContext<DataContext>(options =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-//});
+builder.Services.AddDbContext<DataContext>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
@@ -19,13 +19,13 @@ app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
+	app.UseWebAssemblyDebugging();
 }
 else
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseSwagger();
