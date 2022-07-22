d6243780290b25b742e20d39523b5b87c996667f
Tornike Oniani <dragonforwar@gmail.com>
2022-07-22 21:37:54 +0400

    Added product model and viewmodel and implemented sample product list

---
+++ FinalExam.Domain/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalExam.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string ImageUrl { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+}
---
+++ FinalExam/Client/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalExam.Client.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string ImageUrl { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+}
