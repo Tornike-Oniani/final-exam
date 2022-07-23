21604602edabdc5a65174db41cae7972dca08798
Tornike Oniani <dragonforwar@gmail.com>
2022-07-23 13:59:42 +0400

    installed mediatr and CQRS infrastructure

---
+++ FinalExam.Shared/CQRSInfrastructure.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+
+namespace FinalExam.Shared
+{
+    public class CQRSInfrastructure
+    {
+        public interface IQuery<out TResult> : IRequest<TResult>
+        {
+
+        }
+
+        public interface IQueryHandler<in TQuery, TResult> : IRequestHandler<TQuery, TResult> where TQuery : IQuery<TResult>
+        {
+
+        }
+
+        public interface ICommand<out TResult> : IRequest<TResult>
+        {
+
+        }
+
+        public interface ICommandHandler<in TCommand, TResult> : IRequestHandler<TCommand, TResult> where TCommand : ICommand<TResult>
+        {
+
+        }
+
+        public class CommandExecutionResult
+        {
+            public int Id { get; set; }
+        }
+
+        public abstract class DefaultCommandHandler<TCommand> : ICommandHandler<TCommand, CommandExecutionResult> where TCommand : ICommand<CommandExecutionResult>
+        {
+            public abstract Task<CommandExecutionResult> Handle(TCommand request, CancellationToken cancellationToken);
+        }
+
+        public class Command : ICommand<CommandExecutionResult>
+        {
+
+        }
+    }
+}
